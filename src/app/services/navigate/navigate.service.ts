17b30fc7575b66c6f4460a1a4b33392190a80a0b
Ailko <ailko.claeys@outlook.com>
2022-12-31 10:35:52 +0100

    Mobile first + color

--- src/app/services/navigate/navigate.service.ts
+++ src/app/services/navigate/navigate.service.ts
@@ -1,12 +1,13 @@
-import { Injectable } from '@angular/core';
-import { Router} from "@angular/router";
+import {Injectable} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavigateService {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+  }
 
   public navigate(route: string) {
     this.router.navigateByUrl(route);
